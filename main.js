4dc25ff59f19762fe695cb0b58fa562fe082ce64
jyoti-wp <sim_s006@icloud.com>
2021-05-14 15:03:10 +0530

    final commit

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ const bg = document.querySelector(".bg");
 const loadText = document.querySelector(".loading-text");
 
 let load = 0;
-
 let int = setInterval(blurring, 30);
 
 function blurring() {
   load++;
-
   if (load > 99) {
-    clearInterval(int);
+    setInterval(int);
   }
 
   loadText.innerText = `${load}%`;
